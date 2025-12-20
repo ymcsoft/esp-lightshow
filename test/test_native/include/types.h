3ce750ecf59c8c863209e8d7dae005b3c4b7e80b
user az30314 <user@az30314.com>
2025-12-19 17:23:59 -0800

    fixed compiler warning

--- test/test_native/include/types.h
+++ test/test_native/include/types.h
@@ -9,13 +9,4 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 
-typedef struct PIXEL_COLOR {
-    uint8_t r, g, b;
-    explicit PIXEL_COLOR(uint8_t r=0, uint8_t g=0, uint8_t b=0) {
-        this->r = r;
-        this->b = b;
-        this->g = g;
-    }
-};
-
 #endif //ESP_LIGHTSHOW_TYPES_H
