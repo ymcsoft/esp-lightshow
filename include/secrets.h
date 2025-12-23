869ffac247a2a5a5b5634302fbd550a64e887c8d
user az30314 <user@az30314.com>
2025-12-22 19:38:31 -0800

    Updated secrets.h to use environment variables to set SSID, PASS, DEVICE_KEY and DEVICE_LOGIN_NAME values. Added those variables to platformio.ini

--- include/secrets.h
+++ include/secrets.h
@@ -1,12 +1,34 @@
 //
 // Created by Yuri Moiseyenko on 2025-12-20.
 //
-
 #ifndef ARDUINO_SECRETS_H
 #define ARDUINO_SECRETS_H
-const char DEVICE_LOGIN_NAME[]  = "DEVICE_NAME";
 
-const char SSID[]               = "WIFI_SSID";    // Network SSID (name)
-const char PASS[]               = "WIFI_PASSWORD";    // Network password (use for WPA, or use as key for WEP)
-const char DEVICE_KEY[]  = "DEVICE_KEY";    // Secret device password
-#endif //ARDUINO_SECRETS_H
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#ifdef IOT_DEVICE_ID
+const char DEVICE_LOGIN_NAME[]  = TOSTRING(IOT_DEVICE_ID);
+#else
+const char DEVICE_LOGIN_NAME[]  = "DEVICE_NAME";
+#endif
+
+#ifdef WIFI_SSID
+const char SSID[]               = TOSTRING(WIFI_SSID);
+#else
+const char SSID[]               = "WIFI_SSID";
+#endif
+
+#ifdef WIFI_PASSWORD
+const char PASS[]               = TOSTRING(WIFI_PASSWORD);
+#else
+const char PASS[]               = "WIFI_PASSWORD";
+#endif
+
+#ifdef IOT_DEVICE_PASSWORD
+const char DEVICE_KEY[]         = TOSTRING(IOT_DEVICE_PASSWORD);
+#else
+const char DEVICE_KEY[]         = "DEVICE_KEY";
+#endif
+
+#endif // ARDUINO_SECRETS_H
